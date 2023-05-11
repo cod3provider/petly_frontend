b83e9816f8d2b7e47e9364b7c51d191f13f4f535
Ilya Bondarchuk <cod3provider@gmail.com>
2023-05-11 22:15:46 +0300

    add structure

---
+++ src/utils/theme.jsx
@@ -0,0 +1,20 @@
+export const baseColors = {
+	accentColor: '#54ADFF',
+}
+
+export const theme = {
+	breakpoints: {
+		mobile: {
+			media: '(max-width: 767px)',
+			width: '320px',
+		},
+		tablet: {
+			media: '(min-width: 768px) and (max-width: 1279px)',
+			width: '768px',
+		},
+		desktop: {
+			media: '(min-width: 1280px)',
+			width: '1280px',
+		}
+	}
+}
