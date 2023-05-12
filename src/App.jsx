7e7f6355304d8ab1d3897888bb452a8340078737
NazarKr <nazarok1990@gmail.com>
2023-05-12 17:36:40 +0300

    add AuthForm (Login , RegisterPage, Redux)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx"
 
 function App() {
 
   return (
     <>
       <HomePage />
+      <RegisterPage />
+      <LoginPage/>
     </>
   )
 }
