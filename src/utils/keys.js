5a5698de93be09fe0eeb62ebcbe1c7fe32f2c663
Ilya Bondarchuk <cod3provider@gmail.com>
2023-05-13 15:56:11 +0300

    small fix of package.json

---
+++ src/utils/keys.js
@@ -0,0 +1,19 @@
+const ROUTES = {
+  REGISTER: '/register',
+  LOGIN: '/login',
+  NEWS: '/news',
+  NOTICES: '/notices',
+  FRIENDS: '/friends',
+  USER: '/user',
+};
+
+const BACKEND_ENDPOINTS = {
+  REGISTER: 'auth/register',
+  LOGIN: 'auth/login',
+  LOGOUT: 'auth/logout',
+}
+
+export {
+  ROUTES,
+  BACKEND_ENDPOINTS,
+}
