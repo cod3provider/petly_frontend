80b0ad0575419f82bbd697e0648e88f685d13adf
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-05-13 21:02:59 +0300

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// import Spinner from './pages/Spinner/Spinner';
+// import SharedLayout from './pages/SharedLayout/SharedLayout';
 import HomePage from './pages/HomePage/HomePage.jsx';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
 import AddPet from './components/AddPet/AddPet';
+// import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
-import AddPet2 from './components/AddPet2/AddPet2';
+import './App.css';
+
+// Удалить когда будут компоненты
+const SharedLayout = () => {
+  return <></>;
+};
+
+const Spinner = () => {
+  return <></>;
+};
+
+const NotFoundPage = () => {
+  return <></>;
+};
+// ===================================
 
 function App() {
   return (
-    <>
-      {/* <HomePage />
-      <RegisterPage />
-      <LoginPage />
-      <AddPet /> */}
-      <AddPet2 />
-    </>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/addpet" element={<AddPet />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'styled-components';
+
+import App from './App.jsx';
+
 import './index.css';
+import { theme } from './utils/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
