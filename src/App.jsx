ca317798e80e797471cbd22ee8472e3f939775a8
Ilya Bondarchuk <cod3provider@gmail.com>
2023-05-15 17:24:20 +0300

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import './App.css'
-import HomePage from "./pages/HomePage/HomePage.jsx";
+import './App.css';
+import HomePage from './pages/HomePage/HomePage.jsx';
 import GlobalStyles from './utils/globalStyles.js';
 import LoginPage from './pages/LoginPage/LoginPage';
-import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx"
+import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
 
 function App() {
-
   return (
     <>
       <GlobalStyles />
       <HomePage />
-      // <RegisterPage />
-      // <LoginPage/>
+      <RegisterPage />
+      <LoginPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
