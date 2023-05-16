0a8ca1938d41f5547dfb356a5064741d4e94bf19
Pavlo <pasam9300@gmail.com>
2023-05-16 18:29:52 +0200

    Commit message

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,12 +2,16 @@ import Header from '../Header/Header.jsx';
 
 import PropTypes from 'prop-types';
 import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
 export const SharedLayout = ({ children }) => {
   return (
     <>
       <Header></Header>
-      <Suspense>{children}</Suspense>
+      <Suspense>
+        {children}
+        <Outlet />
+      </Suspense>
     </>
   );
 };
