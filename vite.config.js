1e5fe5537d77559ede3e6941ed74316315e819a9
Ilya Bondarchuk <cod3provider@gmail.com>
2023-05-18 17:14:50 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/petly_frontend',
+  base: '/petly_frontend/',
   // server: {
   //   port: 3000,
   // }
