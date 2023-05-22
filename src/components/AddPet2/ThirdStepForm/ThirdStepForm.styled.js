4dc520fb85c23866d841b145b8e97207a3adfbe6
Enjoy3325 <enjoy3325@gmail.com>
2023-05-22 04:13:42 +0300

    add a little style form add pet

--- src/components/AddPet2/ThirdStepForm/ThirdStepForm.styled.js
+++ src/components/AddPet2/ThirdStepForm/ThirdStepForm.styled.js
@@ -4,15 +4,14 @@ import styled from 'styled-components';
 export const TextareaStyle = styled('input')`
   width: 100%;
   height: 92px;
-  resize: none;
   padding: 8px 0 0 16px;
+  resize: none;
   border: 1px solid #54adff;
   border-radius: 20px;
   ::placeholder {
     font-weight: 400;
     font-size: 14px;
     line-height: 150%;
-
     align-items: center;
     letter-spacing: 0.04em;
     color: #888888;
