1f8ba1cea93e708e1e4ef547b1e0cd35c4b7d15a
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2023-05-24 21:59:12 +0300

    Revert "Revert "Fix styles user page""

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -9,8 +9,9 @@ import UserPets from '../../components/UserPage/PetsData/PetsData';
 import Logout from '../../components/Logout/Logout';
 import { ContainerStyled } from '../../components/common/Container/Container.styled';
 import { SectionStyled } from '../../components/common/Section/Section.styled';
-import { Wrap, UserDiv, Title } from './UserPage.styled';
+import { Wrap, UserDiv, Title, UserWrapper } from './UserPage.styled';
 import { useToggle } from '../../hooks/useToggle';
+import { PetWrapper } from '../../components/UserPage/PetsData/PetsData.styled.jsx';
 import { setIsNewUser } from '../../redux/auth/authSlice';
 import ModalBack from '../../components/Modals/ModalBack';
 import ModalCongrats from '../../components/Modals/ModalCongrats/ModalCongrats';
@@ -36,12 +37,16 @@ function UserPage() {
     <ContainerStyled style={{ background: '#FEF9F9' }}>
       <SectionStyled>
         <UserDiv>
-          <div>
+          <UserWrapper>
             <Title>My information:</Title>
             <Wrap>
               <UserData />
               <Logout onClick={open} />
             </Wrap>
+          </UserWrapper>
+          <PetWrapper>
+            <UserPets />
+          </PetWrapper>
           </div>
           {isNewUser ? (
             <ModalBack onClose={onClose}>
