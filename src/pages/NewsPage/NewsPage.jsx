7db2d000e02c825fc972b0ebd7e579842d2544a5
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-05-26 15:23:31 +0300

    Revert "small fixes"
    This reverts commit 3c63a5ef4556e3b3f97771474db93536f9e04599.

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,97 +1,20 @@
-import { getNews, getNewsSerch } from '../../services/newsApi';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
-import { useState, useEffect } from 'react';
-import { toast } from 'react-toastify';
+import NewsList from './NewsList/NewsList.jsx';
+import { ContainerStyled } from '../../components/common/Container/Container.styled.jsx';
+import { SectionStyled } from '../../components/common/Section/Section.styled.jsx';
 
-import { TitleStyled } from './../../components/common/Title/Title.styled.js';
-import { NewsCardContainer } from './NewsCard/NewsCard.styled.js';
-
-import NoticesSearch from './../../components/NoticesPageComponents/NoticesSearch/NoticesSearch';
-import NoticesPaginationButtons from './../../components/NoticesPageComponents/NoticesPaginationButtons/NoticesPaginationButtons';
-import NewsCard from './NewsCard/NewsCard';
-
-const NewNewsPage = () => {
-  const [query, setQuery] = useState('');
-  const [news, setNews] = useState([]);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 1280);
-  const [limit, setLimit] = useState(isWideScreen ? '12' : '10');
-
-  useEffect(() => {
-    const resizeHandler = () => {
-      setIsWideScreen(window.innerWidth >= 1280);
-    };
-
-    resizeHandler();
-
-    if (isWideScreen) {
-      setLimit('12');
-    } else {
-      setLimit('10');
-    }
-
-    window.addEventListener('resize', resizeHandler);
-
-    return () => {
-      window.removeEventListener('resize', resizeHandler);
-    };
-  }, [isWideScreen]);
-
-  const searchNews = query => {
-    setQuery(query);
-  };
-
-  useEffect(() => {
-    const fetchNewsByQuery = async (query, page, limit) => {
-      try {
-        const response = await getNewsSerch(query, page, limit);
-        setNews(response.data);
-        setTotalPages(response.totalPages);
-      } catch (error) {
-        toast.error(error.message);
-      }
-    };
-
-    const fetchNews = async (page, limit) => {
-      try {
-        const response = await getNews(page, limit);
-        setNews(response.data);
-        setTotalPages(response.totalPages);
-      } catch (error) {
-        toast.error(error.message);
-      }
-    };
-
-    if (query !== '') {
-      fetchNewsByQuery(query, page, limit);
-    }
-
-    if (query === '') {
-      fetchNews(page, limit);
-    }
-  }, [query, page, limit, isWideScreen]);
-
+export default function NewsPage() {
   return (
-    <>
-      <TitleStyled>News</TitleStyled>
-      <NoticesSearch onSubmit={searchNews} />
-      <NewsCardContainer className="news-container">
-        {news.map(newsItem => (
-          <NewsCard key={newsItem._id} news={newsItem} />
-        ))}
-      </NewsCardContainer>
-
-      {!news.length || (
-        <NoticesPaginationButtons
-          currentPage={page}
-          totalPages={totalPages}
-          onPageChange={setPage}
-        />
-      )}
-      {news.length === 0 && <p>not found</p>}
-    </>
+    <HelmetProvider>
+      <SectionStyled>
+        <ContainerStyled>
+          {/*<Helmet>*/}
+          {/*  <title>NewsPage</title>*/}
+          {/*</Helmet>*/}
+          <NewsList />
+        </ContainerStyled>
+      </SectionStyled>
+    </HelmetProvider>
   );
-};
-
-export default NewNewsPage;
+}
